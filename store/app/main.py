a8eba23c58b7a3c4f61d8d18d1f1176ca510cfaf
Nestor J Solera <59302496+NessDisk@users.noreply.github.com>
2024-08-29 14:37:04 -0500

    Ajustes

--- store/app/main.py
+++ store/app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from Controller import ClientController, FacturaController, ProductController
+from Controller import clientcontroller, FacturaController, ProductController
 
 
 
 app = FastAPI()
 
-app.include_router(ClientController.router, prefix="/Client", tags=["Client"])
+app.include_router(clientcontroller.router, prefix="/Client", tags=["Client"])
 app.include_router(FacturaController.router, prefix="/Facture", tags=["Facture"])
 # app.include_router(ProductController.router, prefix="/Product", tags=["Product"])
