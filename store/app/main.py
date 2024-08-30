9f349e3c8a0ba593073b516cbb6dbb16f1ddcd5e
Nestor J Solera <59302496+NessDisk@users.noreply.github.com>
2024-08-30 12:45:59 -0500

    CORS

--- store/app/main.py
+++ store/app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 from Controller import clientcontroller, FacturaController, ProductController
 
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todas las origines, cambia esto según tus necesidades
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos, ajusta según sea necesario
+    allow_headers=["*"],  # Permitir todos los encabezados, ajusta según sea necesario
+)
 
 app.include_router(clientcontroller.router, prefix="/Client", tags=["Client"])
 app.include_router(FacturaController.router, prefix="/Factura", tags=["Factura"])
