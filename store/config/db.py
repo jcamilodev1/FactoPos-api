8239aae419760872e2eae70a7dba1ee250f0a653
jcamilodev2 <123014168+jcamilodev2@users.noreply.github.com>
2024-08-31 14:00:53 -0500

    database

--- store/config/db.py
+++ store/config/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./database.db"  # Cambia esta URL a la de tu base de datos
+DATABASE_URL = "postgresql+psycopg2://factouser:admin123@localhost/mydatabase" # Cambia esta URL a la de tu base de datos
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
