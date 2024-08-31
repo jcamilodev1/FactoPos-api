71cade4ee8bdedca4be81d6c16dd5f04280c33fb
jcamilodev2 <123014168+jcamilodev2@users.noreply.github.com>
2024-08-31 10:58:47 -0500

    config

--- store/config/db.py
+++ store/config/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./database.db"  # Cambia esta URL a la de tu base de datos
+DATABASE_URL = "sqlite:///app/store/database.db"  # Cambia esta URL a la de tu base de datos
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
